c8046c90890fef902bb824278cdf64ac1df45468
David Larsen <davidplarsen@gmail.com>
2020-07-08 22:16:47 -0500

    Binary search

--- binarysearch.js
+++ binarysearch.js
@@ -3,16 +3,20 @@ const test = [0, 1, 21, 33, 45, 45, 65, 71, 72, 73]
 function binarysearch(array, target) {
     let left = 0
     let right = array.length - 1
-    while (left !== right) {
+    while (left <= right) {
         let middle = Math.floor((left + right) / 2)
-        if (array[middle] < target) {
+        if (array[middle] === target) {
+            return true
+        }
+        else if (array[middle] < target) {
             left = middle + 1
         } else if (array[middle] > target) {
             right = middle - 1
         }
     }
 
-    return array[left]
+    return false
+
 }
 
 console.log(binarysearch(test, 33))
